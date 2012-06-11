d7b76ade021b812fe44b1199cee740d3a8185bd6
BoD <BoD@JRAF.org>
2012-06-11 18:12:46 +0200

    Initial commit.

---
+++ src/org/jraf/android/latoureiffel/MainActivity.java
@@ -0,0 +1,22 @@
+package org.jraf.android.latoureiffel;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+public class MainActivity extends SherlockActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+}
